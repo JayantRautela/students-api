9fe7ba5c55bc7ead62e7e16b4a45484a3c5f33cc
JayantRautela <jayantrautela0205@gmail.com>
2026-08-04 00:51:26 +0530

    add validation tags to Student fields

--- internal/types/types.go
+++ internal/types/types.go
@@ -2,7 +2,7 @@ package types
 
 type Student struct {
 	Id    int
-	Email string
-	Name  string
-	Age   int
+	Email string `validate:"required"`
+	Name  string `validate:"required"`
+	Age   int `validate:"required"`
 }
