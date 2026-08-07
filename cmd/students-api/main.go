d43654a6038b0d67cf62c85bf35056ba0312ac79
JayantRautela <jayantrautela0205@gmail.com>
2026-08-08 01:07:13 +0530

    feat: implement SQLite storage with student creation functionality

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/JayantRautela/students-api/internal/config"
 	"github.com/JayantRautela/students-api/internal/http/handlers/student"
+	"github.com/JayantRautela/students-api/internal/storage/sqlite"
 )
 
 // main entry point
@@ -19,11 +20,20 @@ func main() {
 	// load config
 	cfg := config.MustLoad()
 
+	// databse setup
+	storage, err := sqlite.New(cfg)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	slog.Info("Storage initialized", slog.String("env", cfg.Env), slog.String("version", "1.0.0"))
+
 	// router setup
 	router := http.NewServeMux() // gives a new Server Mux (router)
 
 	// routers
-	router.HandleFunc("POST /api/students", student.New())
+	router.HandleFunc("POST /api/students", student.New(storage))
 
 	// server setup
 	server := http.Server {
@@ -54,7 +64,7 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
 	defer cancel()
 
-	err := server.Shutdown(ctx)
+	err = server.Shutdown(ctx)
 
 	if err != nil {
 		slog.Error("Failed to shutdown server", slog.String("error", err.Error()))
