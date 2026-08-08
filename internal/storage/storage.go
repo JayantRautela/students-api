0cab1918a83baffa108ece6a69ef53007184dcf2
JayantRautela <jayantrautela0205@gmail.com>
2026-08-08 13:25:35 +0530

    feat: add GET endpoint for fetching all students and implement corresponding storage method

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -5,4 +5,5 @@ import "github.com/JayantRautela/students-api/internal/types"
 type Storage interface {
 	CreateStudent(name string, email string, age int) (int, error)
 	GetStudentById(id int) (types.Student, error)
+	GetStudents() ([]types.Student, error)
 }
